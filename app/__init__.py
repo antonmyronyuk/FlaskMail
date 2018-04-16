44923c4b437845d749ba7dbb153700163de605ab
Anton Myronyuk <antonmyronyuk@gmail.com>
2018-04-16 18:10:33 +0300

    added login

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,13 @@ from config import Config
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
 mail = Mail(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,55 @@
-from app import app, mail
-from flask import render_template, request, url_for, flash
+from app import app, mail, login
+from flask import render_template, request, url_for, flash, redirect
 from flask_mail import Message
 from smtplib import SMTPException
 from werkzeug.datastructures import MultiDict
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
+from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    return render_template('form.html')
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        flash('Successfully logged in !')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('Successfully logged out')
+    return redirect(url_for('login'))
 
 
 @app.route('/help')
 def help_send():
     return render_template('help.html')
+
+
+@app.route('/checker')
+def checker():
+    return render_template('form.html')
 
 
 @app.route('/send', methods=['POST'])
