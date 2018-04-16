fb87a17de01ca323ba2326063d18f8f9d40c93f5
Anton Myronyuk <antonmyronyuk@gmail.com>
2018-04-16 20:12:06 +0300

    markup fix in flash messages

--- app/routes.py
+++ app/routes.py
@@ -33,7 +33,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        flash('Successfully logged in !')
+        flash('Successfully logged in!')
         return redirect(next_page)
     return render_template('login.html', title='Log In', form=form)
 
