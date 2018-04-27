7565bc49aac1364d31e9e3957998ef1156863123
Anton Myronyuk <antonmyronyuk@gmail.com>
2018-04-27 13:27:39 +0300

    added email_notifications boolean field in user table

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     token = db.Column(db.String(50))
+    email_notifications = db.Column(db.Boolean)
     messages = db.relationship('Message', backref='receiver', lazy='dynamic')
 
     # call only after adding user email
